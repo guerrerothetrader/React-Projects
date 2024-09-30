39c7865a5d16eb32b4ab98f12db82675798153dd
Alberto G <126378351+guerrerothetrader@users.noreply.github.com>
2024-09-30 08:52:19 +0200

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+import Services from './Services';
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Footer,
+    CustomerReviews,
+    Subscribe,
+    SpecialOffer,
+    Services
+}
